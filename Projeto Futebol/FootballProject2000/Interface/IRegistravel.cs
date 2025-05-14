49b1ecfdc71fd26db20af88e303e7e8bfbd9d485
Leonardo Rossetti Francatto <leo.francatto@gmail.com>
2025-05-14 20:17:18 -0300

    fix

--- Projeto Futebol/FootballProject2000/Interface/IRegistravel.cs
+++
@@ -1,6 +0,0 @@
-namespace Models;
-public interface IRegistravel
-{
-    void RegistrarInteressado(string nome);
-    List<string> ListarInteressados();
-}
--- obj/Debug/net9.0/ERP-do-Baba-Gropo-11.AssemblyInfo.cs
+++ obj/Debug/net9.0/ERP-do-Baba-Gropo-11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP-do-Baba-Gropo-11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f62ff3bef15ca46516ec549146a226a3acc809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP-do-Baba-Gropo-11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP-do-Baba-Gropo-11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
